33796818d3b556e04c74c2dc8d1ab86caa53384d
Aaron <aaronpiolang@gmail.com>
2019-11-26 21:31:07 +0000

    comparing arrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,6 +8,18 @@ const assertEqual = (actual, expected) => {
   }
 }
 
-// TEST CODE
-assertEqual("yo", "yo");
-assertEqual(1, 1);
+
+const eqArrays = (arr1, arr2) => {
+
+  for (i = 0 ; i < arr1.length ; i++) {
+    
+    if (arr1[i] !== arr2[i]) {
+      return false 
+    }
+  }
+  return true
+}
+
+
+// Testing
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
