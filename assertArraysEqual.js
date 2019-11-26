2c7e3a0a0df1fb5d180eeca20e0789c58eae1937
Aaron <aaronpiolang@gmail.com>
2019-11-26 22:02:33 +0000

    new file

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+
+const assertArraysEqual = (arr1, arr2) => {
+  let result
+  for (i = 0 ; i < arr1.length ; i++) {
+    if (arr1[i] !== arr2[i]) {
+       result = false
+    }
+  }
+  if (arr1.length !== arr2.length) {
+    result = false
+  }
+  if (result === false) {
+    console.log(`🛑 Assertion Failed: ${result}`)
+    return
+  }
+  result = true
+  console.log(`✅ Assertion Passed: ${result}`)
+  return
+}
