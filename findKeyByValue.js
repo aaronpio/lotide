fa5fa6ee37f03a92c2898fb2dce8e0190a28177f
Aaron <aaronpiolang@gmail.com>
2019-11-27 18:49:25 +0000

    new func

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`)
+    return
+  } else {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    return
+  }
+}
+
+const findKeyByValue = (obj, value) => {
+  for (let key in obj) {
+    if (obj[key] === value) {
+      return key
+    }
+  }
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
