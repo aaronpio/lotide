4aaf6beed833438b3e4e4d4bf487c8d223bb1f1b
Aaron <aaronpiolang@gmail.com>
2019-11-27 17:44:05 +0000

    countOnly function added

---
+++ countOnly.js
@@ -0,0 +1,50 @@
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`)
+    return
+  } else {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    return
+  }
+}
+
+
+const countOnly = (allItems, itemsToCount) => {
+  const results = {}
+
+  allItems.forEach((item) => {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1
+      } else {
+        results[item] = 1
+      }
+
+    }
+  })
+  return results
+}
+
+const without = (source, itemsToRemove) => {
+  return source.filter((e) => !itemsToRemove.includes(e))
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+console.log(result1)
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
