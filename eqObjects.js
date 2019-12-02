36329cdee026eef5433b06e6aef8d5eaab641ab3
Aaron <aaronpiolang@gmail.com>
2019-12-02 22:22:06 +0000

    added eqArrays export to eqObjects file. updated version number

--- eqObjects.js
+++ eqObjects.js
@@ -1,15 +1,4 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const { eqArrays } = require("./index");
 
 const eqObjects = (obj1, obj2) => {
   const obj1Keys = Object.keys(obj1);
